e3f066836ffa00e140c4ef0833f2aca66c80bf97
ali.teeney <alistair.teeney@octoenergy.com>
2018-01-20 20:56:09 +0000

    Adds tunes script to interact with songs DB

---
+++ tunes.py
@@ -0,0 +1,38 @@
+import logging
+from datetime import datetime, timedelta
+import sqlite3 as sql
+
+def get_tune(db_path, is_christmas):
+    logging.info('Getting song info')
+    today = datetime.now()
+    db = sql.connect(db_path)
+    c = db.cursor()
+    week_ago = today - timedelta(days=7)
+    xmas_filter = 'AND christmas' if is_christmas else ''
+    c.execute('''
+    SELECT
+        url, name, artist_name
+    FROM songs
+    WHERE
+        DATE(last_played_date) < DATE('{}')
+        OR last_payed_date IS NULL
+        {}
+    ORDER BY RANDOM()
+    LIMIT 1;
+    '''.format(week_ago.strftime('%Y-%m-%d'), xmas_filter))
+    song = c.fetchone()
+    song_url = song[0]
+    song_name = song[1]
+    song_artist = song[2]
+
+    update_last_played(db, song_url, today)
+    logging.info('Today\'s song: {} - {}'.format(song_artist, song_name))
+    return song_artist, song_name, song_url
+
+def update_last_played(db, song_url, date):
+    c = db.cursor()
+    c.execute('''
+    UPDATE songs SET last_played_date = '{}' WHERE url = '{}'
+    '''.format(date.strftime('%Y-%m-%d'), song_url))
+    db.commit()
+
